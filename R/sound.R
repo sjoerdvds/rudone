b01d03df6158083e5d84407f0c58e239a3dd7ec0
sjoerdvds <s.j.vanderspoel@utwente.nl>
2016-05-24 14:01:45 +0200

    Update gitignore

--- R/sound.R
+++ R/sound.R
@@ -1,6 +1,6 @@
 #' @section Sound: Notify by playing a sound
 #' @rdname notification
-#' @param file Location of sound file to play
-sound <- function(file = "extdata/chime.wav"){
-
+#' @param sound_file Location of sound file to play
+sound <- function(sound_file = "extdata/chime.wav"){
+  notification <- list(sound_file = file)
 }
